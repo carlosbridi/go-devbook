d5774fb2d9e8842084370e4b683f4bdd3f162bd0
carlosbridi <carlosbridi@gmail.com>
2022-10-28 09:01:20 -0300

    Struct and refect packages

--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -1,11 +1,25 @@
 package rotas
 
-import "net/http"
+import (
+	"net/http"
 
-//Rota representa estrutura de Rota
+	"github.com/gorilla/mux"
+)
+
+// Rota representa estrutura de Rota
 type Rota struct {
 	URI                string
 	Metodo             string
 	Funcao             func(w http.ResponseWriter, r *http.Request)
 	RequerAutenticacao bool
+}
+
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	}
+
+	return r
 }
--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,8 +1,13 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"api/src/router/rotas"
 
-//"Gera routas"
+	"github.com/gorilla/mux"
+)
+
+// "Gera routas"
 func Gerar() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+	return rotas.Configurar(r)
 }
