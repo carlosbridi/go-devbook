d64aef79277aa8dbd90022e5dc0eb980206d3a5e
carlosbridi <carlosbridi@gmail.com>
2022-11-27 19:51:22 -0300

    Criação de repositorio de usuário, método para criar usuário no DB e classe de resposta

---
+++ api/src/controllers/usuarios.go
@@ -0,0 +1,59 @@
+package controllers
+
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuario.ID, erro = repositorio.Criar(usuario)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, usuario)
+	//w.Write([]byte(fmt.Sprintf("Id inserido: %d", usuarioId)))
+
+}
+
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscar usuários"))
+}
+
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscar usuário"))
+}
+
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Atualizando usuário"))
+}
+
+func ExcluirUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Excluindo usuário"))
+}
