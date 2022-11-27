b833e2227e817c680933b599530f4a653ea1aff3
carlosbridi <carlosbridi@gmail.com>
2022-11-27 20:02:42 -0300

    Formatação de dados da entrada do usuário e normalização da informação

--- api/src/modelos/Usuario.go
+++ api/src/modelos/Usuario.go
@@ -1,8 +1,12 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
-//Usuário
+// Usuário
 type Usuario struct {
 	ID       uint64    `json:"id,omitempty"`
 	Nome     string    `json:"nome,omitempty"`
@@ -10,4 +14,41 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
+
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("Nome é obrigatório")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("Nick é obrigatório")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("Email é obrigatório")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("Senha é obrigatório")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Senha = strings.TrimSpace(usuario.Senha)
+	usuario.Email = strings.TrimSpace(usuario.Email)
+
 }
