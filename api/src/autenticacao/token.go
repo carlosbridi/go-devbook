57c1f2c603dc287f3cc4ef97c9e37dfc833cd783
Carlos Bridi <carlosbridi@gmail.com>
2023-02-21 17:17:20 -0300

    Authorization and token verification

--- api/src/autenticacao/token.go
+++ api/src/autenticacao/token.go
@@ -2,6 +2,9 @@ package autenticacao
 
 import (
 	"api/src/config"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -15,5 +18,32 @@ func CriarToken(usuarioID uint64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
 
 	return token.SignedString([]byte(config.SecretKey))
+}
 
+func ValidarToken(r *http.Request) error {
+	tokenString := extrairToken(r)
+	token, erro := jwt.Parse(tokenString, retornarChaveVerificacao)
+	if erro != nil {
+		return erro
+	}
+
+	fmt.Println(token)
+	return nil
+}
+
+func extrairToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+	return ""
+}
+
+func retornarChaveVerificacao(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Método de assinatura inesperado! %v", token.Header["alg"])
+	}
+
+	return config.SecretKey, nil
 }
