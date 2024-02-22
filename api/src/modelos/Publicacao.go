2a7df8e1faaea29c1848e66e3f1dd65f72e77e60
Carlos Bridi <carlosbridi@gmail.com>
2024-02-21 21:43:35 -0300

    Code missing

--- api/src/modelos/Publicacao.go
+++ api/src/modelos/Publicacao.go
@@ -1,6 +1,11 @@
 package modelos
 
-import "time"
+import (
+	"api/src/modelos"
+	"errors"
+	"strings"
+	"time"
+)
 
 // Modelo de publicação feita por um usuário
 type Publicacao struct {
@@ -11,4 +16,28 @@ type Publicacao struct {
 	AutorNick string    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"` // publicacao sem curtida não iriam ter no json de saída, e como o valor padrão é zero, pode ficar sem
 	CriadaEm  time.Time `json:"criadaEm,omitempty"`
+}
+
+func (publicacao *modelos.Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+	return nil
+}
+
+func (publicacao *modelos.Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("Título é obrigatório")
+	}
+	if publicacao.Conteudo == "" {
+		return errors.New("Conteúdo é obrigatório")
+	}
+	return nil
+}
+
+func (publicacao *modelos.Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
